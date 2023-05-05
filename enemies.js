28f5fe7a070763dfbf62d6633a3afe86b1a6662e
perry1224 <perry.w1224@gmail.com>
2023-05-04 22:52:46 -0400

    flying enemy

--- enemies.js
+++ enemies.js
@@ -5,24 +5,55 @@ class Enemy {
     this.fps = 20;
     this.frameInterval = 1000/this.fps;
     this.frameTimer = 0;
+    this.markedForDeletion = false
   }
-  update() {
-
+  update(deltaTime) {
+    //movement
+    this.x -= this.speedX;
+    this.y -= this.speedY;
+    if (this.frameTimer > this.frameInterval){
+      this.frameTimer = 0;
+      if(this.frameX < this.maxFrame) this.frameX++;
+      else this.frameX = 0;
+    } else {
+      this.frameTimer +=deltaTime;
+    }
+    //check if off screen
+    if (this.x + this.width < 0) this.markedForDeletion = true;
   }
-  draw() {
+  draw(context) {
+    context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height)
 
   }
 
 }
 
-class FlyingEnemy extends Enemy {
+export class FlyingEnemy extends Enemy {
+  constructor(game) {
+    super();
+    this.game = game;
+    this.width = 60;
+    this.height = 44;
+    this.x = this.game.width + Math.random() * this.game.width *0.5;
+    this.y = Math.random() * this.game.height * 0.5;
+    this.speedX = Math.random() + 1;
+    this.speedY = 0;
+    this.maxFrame = 5;
+    this.image = document.getElementById('enemy_fly');
+    this.angle= 0;
+    this.va = Math.random() * 0.1 + 0.1
+  }
+  update(deltaTime) {
+    super.update(deltaTime);
+    this.angle += this.va;
+    this.y += Math.sin(this.angle)
+  }
+}
+
+export class GroundEnemy extends Enemy {
 
 }
 
-class GroundEnemy extends Enemy {
+export class ClimbingEnemy extends Enemy {
 
-}
-
-class ClimbingEnemy extends Enemy {
-  
 }
