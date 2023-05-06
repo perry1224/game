52275e26c5f6513ea6ef2ca696bd11cbedd64c88
perry1224 <perry.w1224@gmail.com>
2023-05-05 23:36:57 -0400

    dust and fire particles

--- main.js
+++ main.js
@@ -23,11 +23,15 @@ class Game {
     this.input = new InputHandler(this)
     this.UI = new UI(this)
     this.enemies = [];
+    this.particles = [];
+    this.maxParticles = 100;
     this.enemyTimer = 0;
     this.enemyInterval = 1000;
     this.debug = true;
     this.score = 0;
     this.fontColor = 'black';
+    this.player.currentState = this.player.states[0];
+    this.player.currentState.enter()
   }
   update(deltaTime) {
     this.background.update()
@@ -42,7 +46,17 @@ class Game {
     this.enemies.forEach(enemy => {
       enemy.update(deltaTime)
       if(enemy.markedForDeletion) this.enemies.splice(this.enemies.indexOf(enemy), 1)
-    })
+    });
+    //handle particles
+    this.particles.forEach((particle, index) => {
+      particle.update();
+      if (particle.markedForDeletion) this.particles.splice(index, 1)
+
+    });
+    if (this.particles.length > this.maxParticles) {
+      this.particles = this.particles.slice(0, this.maxParticles) //only allow first 50 particles in this array.
+    }
+
   }
   draw(context) {
     this.background.draw(context)
@@ -50,17 +64,20 @@ class Game {
     this.enemies.forEach(enemy => {
       enemy.draw(context)
     });
+    this.particles.forEach(particle => {
+      particle.draw(context)
+    })
     this.UI.draw(context);
   }
   addEnemy(){
     if (this.speed > 0 && Math.random() < 0.5) this.enemies.push(new GroundEnemy(this));
     else if (this.speed > 0) this.enemies.push(new ClimbingEnemy(this))
     this.enemies.push(new FlyingEnemy(this))
-    console.log(this.enemies)
+   
   }
 }
 const game = new Game(canvas.width, canvas.height);
-console.log(game);
+
 let lastTime = 0;
 
 function animate(timeStamp) {
